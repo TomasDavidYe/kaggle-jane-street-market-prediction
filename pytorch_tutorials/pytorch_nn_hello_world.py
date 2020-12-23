f5546ae485d3a8d73d477582283a2fb309a759fc
Tomas Ye <tomaye@amazon.com>
2020-12-23 22:16:07 +0100

    [DEEP LEARNING WITH PYTORCH] Chapter 6 -> Implementing an MLP to solve the original linear problem

--- pytorch_tutorials/pytorch_nn_hello_world.py
+++ pytorch_tutorials/pytorch_nn_hello_world.py
@@ -1,11 +1,12 @@
 import torch
 import torch.nn as nn
+from torch import optim
 import matplotlib.pyplot as plt
 
 t_c = [0.5, 14.0, 15.0, 28.0, 11.0, 8.0, 3.0, -4.0, 6.0, 13.0, 21.0]
 t_u = [35.7, 55.9, 58.2, 81.9, 56.3, 48.9, 33.9, 21.8, 48.4, 60.4, 68.4]
-t_c = torch.tensor(t_c)
-t_u = torch.tensor(t_u)
+t_c = torch.tensor(t_c).unsqueeze(1)
+t_u = torch.tensor(t_u).unsqueeze(1)
 
 plt.scatter(x=t_u, y=t_c)
 plt.show()
@@ -16,4 +17,6 @@ def reshape(x):
 
 
 linear_model = nn.Linear(1, 1)
-print(linear_model(reshape(t_u)))
+optimizer = optim.SGD(
+    linear_model.parameters(),
+    lr=1e-2)
