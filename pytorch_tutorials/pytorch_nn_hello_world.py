1baf63cb46bb732ed98e894b12add86799f24d59
Tomas Ye <tomaye@amazon.com>
2020-12-24 23:36:31 +0100

    [DEEP LEARNING WITH PYTORCH] Chapter 6 -> Implementing an MLP to solve the original linear problem

--- pytorch_tutorials/pytorch_nn_hello_world.py
+++ pytorch_tutorials/pytorch_nn_hello_world.py
@@ -16,7 +16,37 @@ def reshape(x):
     return x.reshape(x.shape[0], 1)
 
 
+def training_loop(n_epochs, optimizer, model, loss_fn, t_u_train, t_u_val,
+                  t_c_train, t_c_val):
+    for epoch in range(1, n_epochs + 1):
+        t_p_train = model(t_u_train)
+        loss_train = loss_fn(t_p_train, t_c_train)
+        t_p_val = model(t_u_val)
+
+        loss_val = loss_fn(t_p_val, t_c_val)
+        optimizer.zero_grad()
+        loss_train.backward()
+        optimizer.step()
+
+        print(f"Epoch {epoch}, Training loss {loss_train.item():.4f},"
+              f" Validation loss {loss_val.item():.4f}")
+
+
+t_un = 0.1 * t_u
+t_cn = 0.1 * t_c
+
 linear_model = nn.Linear(1, 1)
-optimizer = optim.SGD(
-    linear_model.parameters(),
-    lr=1e-2)
+optimizer = optim.SGD(linear_model.parameters(), lr=1e-2)
+training_loop(
+    n_epochs=1000,
+    optimizer=optimizer,
+    model=linear_model,
+    loss_fn=nn.MSELoss(),
+    t_u_train=t_un,
+    t_u_val=t_un,
+    t_c_train=t_cn,
+    t_c_val=t_cn)
+
+print()
+print(linear_model.weight)
+print(linear_model.bias)
